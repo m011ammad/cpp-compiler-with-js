5a4669fcec365c81cc915fcf1266a5b3d6653d77
mohammadsjh <mohammadsjh@gmail.com>
2021-09-22 10:58:05 +0330

    Refactor(): Refactor Buffer class move codes to from constructor to methods and update properties and update constructor

--- class/bufferClass.js
+++ class/bufferClass.js
@@ -1,21 +1,29 @@
 class Buffer {
+
+  // properties
+  forward = 0;
+  lexemeBegining = 0;
+
   constructor(input) {
-    var forward = 0;
-    var lexemeBegining = 0;
     this.input = input;
-    this.nextChar = function () {
-      return input[forward++];
-    };
-    this.takeLexeme = function () {
-      let lexeme = input.substring(lexemeBegining, forward);
-      lexemeBegining = forward;
-      return lexeme;
-    };
-    this.deleteCurrentLexeme = function () {
-      forward = lexemeBegining;
-    };
-    this.retract = function () {
-      forward--;
-    };
   }
+
+  nextChar() {
+    return input[forward++];
+  };
+
+  takeLexeme() {
+    let lexeme = input.substring(lexemeBegining, forward);
+    lexemeBegining = forward;
+    return lexeme;
+  };
+
+  deleteCurrentLexeme() {
+    forward = lexemeBegining;
+  };
+  
+  retract() {
+    forward--;
+  };
+
 }
